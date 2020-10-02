7920291cf6006cb02135131aca42634141369b3b
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-02 16:09:26 +0200

    dipslay cards from cardImageUrl prop

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,54 @@
 <template>
-  <div>
-    
+
+  <div v-if="cardImageUrls.length" class="container">
+    <p class="card" v-for="( cardImageUrl , index ) in cardImageUrls" :key="index" :style="backgroundImages[index]"></p>
   </div>
+
+  <div v-else>
+    <p class="card card-back"></p>
+  </div>
+  
 </template>
 
 <script>
 export default {
+
   name: 'Card',
+
+  props:{
+    cardImageUrls: {
+      type: Array,
+      required: true
+    }
+  },
+
+  computed:{
+    backgroundImages(){
+      // map url to { backgroundImage: url('url') } objects for style binding
+      return this.cardImageUrls.map( url => { return {backgroundImage: "url('" + url + "')"} })
+    }
+  }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
+.container{
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card{
+  width: 254px;
+  height: 352px;
+  margin: 10px;
+  background-size: cover;
+  background-image: url('../assets/card-back.jpg');
+
+  &-back{
+    background-image: url('../assets/card-back.jpg');
+  }
+}
 
 </style>
