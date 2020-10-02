ae94576b5e8647bdb540ac1de266538427e03798
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-02 11:58:06 +0200

    add search form and emit event

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,60 @@
 <template>
-    <header>
-        <p><img alt="Magic the Gathering logo" src="../assets/logo.png"></p>    
-    </header>
+<header>
+    <p><img src="../assets/logo.png" alt="Magic the Gathering logo" width="640" height="401"></p>
+
+    <form @submit.prevent="cardNameSubmitted">
+        <input id="cardName" name="cardName" type="text" v-model="cardName"><br/>
+        <input type="submit" value="Rechercher une carte">
+    </form>
+</header>
 </template>
 
 <script>
 export default {
   name: 'Header',
+
+  data(){
+      return {
+          cardName: ''
+      }
+  },
+
+  methods:{
+      cardNameSubmitted(){
+          this.$emit( 'card-name-search' , this.cardName )
+      }
+  }
+
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+header{
+    display: flex;
+    align-items: center;
+    margin: 20px;
 
+    p{
+        width: 200px;
+        margin-right: 50px;
+
+        img{
+            width: 100%;
+            height: auto;
+        }
+    }
+
+    form{
+
+        input{
+            width: 200px;
+            height: 30px;
+            font-size: 18px;
+
+            &#cardName{
+                margin-bottom: 10px;
+            }
+        }
+    }
+}
 </style>
