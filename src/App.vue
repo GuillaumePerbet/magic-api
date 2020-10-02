a8a749564fbf4d85c08d1f57dade37231e5a759d
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-02 11:08:29 +0200

    move logo in Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <Header></Header>
   <Card></Card>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    Card
+    Card,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <p><img alt="Magic the Gathering logo" src="../assets/logo.png"></p>    
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+
+</style>
